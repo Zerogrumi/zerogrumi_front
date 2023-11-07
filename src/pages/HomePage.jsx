aed0a7b33bf1b52c553e47be529fd3c9c546b1c6
YunKyeong <alzkdpffk4314@gmail.com>
2023-11-07 23:43:36 +0900

    홈화면디자인(하단바 제외)

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,8 +1,41 @@
 import React from "react";
+import styled from "styled-components";
+import ReactDOM from "react-dom";
+import Hot from "../components/HomePage/Hot";
+import CategoryList from "../components/HomePage/CategoryList";
 export default function HomePage() {
   return (
     <>
-      <h1>HomePage</h1>
+      <PageContainer>
+        <TopLine></TopLine>
+        <StyleContainer>
+          <Hot></Hot>
+          <CategoryList />
+          <CategoryList />
+        </StyleContainer>
+      </PageContainer>
     </>
   );
 }
+
+const PageContainer = styled.div`
+  padding-top: 38px;
+  width: 375px;
+  height: 812px;
+  background: #96c6ba;
+  margin: auto;
+`;
+
+const StyleContainer = styled.div`
+  width: 375px;
+  margin: auto;
+`;
+
+const TopLine = styled.div`
+  position: absolute;
+  top: 22px;
+  height: 10px;
+  width: 375px;
+  background-color: #ef9f38;
+  margin: auto;
+`;
