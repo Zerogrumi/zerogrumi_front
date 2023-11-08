bcf12e028f62e38741daac329ff0e33f85de8047
YunKyeong <alzkdpffk4314@gmail.com>
2023-11-09 01:15:43 +0900

    홈화면(footer제외)

---
+++ src/data/writtenPostData.jsx
@@ -0,0 +1,48 @@
+export const travelData = [
+  {
+    id: 1,
+    title: "길거리 음식도 제로웨이스트 하자",
+    date: Date("2023-11-02"),
+    author: {
+      name: "김미르",
+      zeroGrade: 4,
+      userImg: "../../images/person.svg",
+    },
+    내용: "이것저것맛있겠다 우와!! 진짜 쓰레기를 없애는건 진짜 대단하구나!",
+    category: "JP",
+    heart: 18,
+    commentCount: 2,
+    scrap: 1,
+  },
+  {
+    id: 2,
+    title: "길거리 음식도 제로웨이스트 하자",
+    date: Date("2023-11-02"),
+    author: {
+      name: "김미르",
+      zeroGrade: 4,
+      userImg: "../../images/person.svg",
+      //이것도 따로 유저정보에서 불러와야되나? 잘 모르겠다
+    },
+    내용: "이것저것맛있겠다 우와!! 진짜 쓰레기를 없애는건 진짜 대단하구나!",
+    category: "JP",
+    heart: 18,
+    commentCount: 2,
+    scrap: 1,
+  },
+  {
+    id: 3,
+    title: "길거리 음식도 제로웨이스트 하자",
+    date: Date("2023-11-02"),
+    author: {
+      name: "김미르",
+      zeroGrade: 4,
+      userImg: "../../images/person.svg",
+    },
+    content: "이것저것맛있겠다 우와!! 진짜 쓰레기를 없애는건 진짜 대단하구나!",
+    category: "JP",
+    heart: 18,
+    commentCount: 2,
+    scrap: 1,
+  },
+];
