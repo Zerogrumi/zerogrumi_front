bd7d1b73c0ac03f623ca85cbb55cb176f03e7c68
yerimi00 <fhddl1019@naver.com>
2023-11-10 21:47:35 +0900

    커뮤니티랑 카드 수정

---
+++ src/components/Card.js
@@ -0,0 +1,50 @@
+import React, { useState } from "react";
+import styles from "../css/CommunityPage.module.css";
+
+const Card = () => {
+  const [likeCount, setLikeCount] = useState(0);
+  const [saveCount, setSaveCount] = useState(0);
+
+  const handleLikeClick = () => {
+    setLikeCount((prevCount) => prevCount + 1);
+  };
+
+  const handleSaveClick = () => {
+    setSaveCount((prevCount) => prevCount + 1);
+  };
+
+  return (
+    <>
+      <div className={styles.box}>
+        <div className={styles.user}>
+          <img className={styles.userImg}></img>
+          <div className={styles.userName}>
+            <div style={{ width: "100%", fontSize: "18px", fontWeight: "2px" }}>
+              환경이좋아님
+            </div>
+            제로등급: OOOXO
+          </div>
+        </div>
+        <div className={styles.userContentbox}>
+          <div className={styles.subtitle}>길거리 음식도 제로웨이스트하자</div>
+          <div className={styles.subcontent}>
+            길거리 음식 사먹을때 일회용품에 담아주니까 낭비가 되는거 같아서
+          </div>
+        </div>
+        <div className={styles.recommendBar}>
+          <div className={styles.recommend}>
+            공감 {likeCount} 저장 {saveCount} 댓글 2
+          </div>
+          <button className={styles.recommendBtn} onClick={handleLikeClick}>
+            공감
+          </button>
+          <button className={styles.saveBtn} onClick={handleSaveClick}>
+            저장
+          </button>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Card;
--- src/pages/CommunityPage.js
+++ src/pages/CommunityPage.js
@@ -1,5 +1,6 @@
 import React from "react";
 import styles from "../css/CommunityPage.module.css";
+import Card from "../components/Card";
 import Footer from "../components/Footer";
 import { Link } from "react-router-dom";
 
@@ -10,32 +11,7 @@ const Community = () => {
         <div className={styles.nav}>
           <div className={styles.title}>길거리 음식도 제로웨이스트하자</div>
         </div>
-        <div className={styles.box}>
-          <div className={styles.user}>
-            <img className={styles.userImg}></img>
-            <div className={styles.userName}>
-              <div
-                style={{ width: "100%", fontSize: "18px", fontWeight: "2px" }}
-              >
-                환경이좋아님
-              </div>
-              제로등급 : OOOXO
-            </div>
-          </div>
-          <div className={styles.userContentbox}>
-            <div className={styles.subtitle}>
-              길거리 음식도 제로웨이스트하자
-            </div>
-            <div className={styles.subcontent}>
-              길거리 음식 사먹을때 일회용품에 담아주니까 낭비가 되는거 같아서
-            </div>
-          </div>
-          <div className={styles.recommendBar}>
-            <div className={styles.recommend}>공감 1 저장 1 댓글 2</div>
-            <button className={styles.recommendBtn}>공감</button>
-            <button className={styles.saveBtn}>저장</button>
-          </div>
-        </div>
+        <Card />
         <div className={styles.comment}>
           오 그건 생각 못했네 좋은 방법 공유 고마워!
         </div>
